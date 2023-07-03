5f3ce4145fb19d58ff0425f4ea3110696a92cec8
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-07-03 07:58:53 +0700

    feat: add google tag analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -15,6 +16,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script async strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.FIREBASE_MEASUREMENT_ID}`} />
+        <Script id="ga-id" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.FIREBASE_MEASUREMENT_ID}');
+          `
+        }}/>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
