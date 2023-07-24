e2e898a4eb6cd1e32e4aeb79de95df202c37a830
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-07-24 21:56:53 +0700

    style: add padding horizontal and vertical

--- src/components/ui/button/ButtonNavBar.tsx
+++ src/components/ui/button/ButtonNavBar.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const ButtonNavBar = ({ children }: Props) => {
     return (
-        <button className="text-lg leading-7 font-medium text-white duration-300 hover:bg-clip-text hover:bg-gradient-to-r hover:from-blue-500 hover:to-[#8B5CF6] hover:text-transparent after:block after:border after:border-blue-500 after:transition-transform after:duration-300 after:scale-x-0 after:hover:scale-x-100 after:origin-right after:hover:origin-left">
+        <button className="text-lg px-4 py-2 leading-7 font-medium text-white duration-300 hover:bg-clip-text hover:bg-gradient-to-r hover:from-blue-500 hover:to-[#8B5CF6] hover:text-transparent after:block after:border after:border-blue-500 after:transition-transform after:duration-300 after:scale-x-0 after:hover:scale-x-100 after:origin-right after:hover:origin-left">
             {children}
         </button>
     )
