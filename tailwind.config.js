9a08a0e6ca541d9534f1f21e770d0249e2271705
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-07-24 21:52:54 +0700

    chore: extend drop shadow elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      dropShadow: {
+        'glow-sm': '0 0 10px rgba(99, 102, 241, 1)',
+        'glow-md': '0 0 20px rgba(99, 102, 241, 1)',
+        'glow-lg': '0 0 30px rgba(99, 102, 241, 1)',
+      },
     },
   },
   plugins: [],
