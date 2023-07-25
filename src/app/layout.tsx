134ae91efa5d90797b892b96b17121527c14ddfe
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-07-25 21:56:11 +0700

    Add footer (#4)
    * chore(deps): add tsparticles engine for react
    
    * style: change scrollbar style
    
    * asset: add aurora svg
    
    * chore: import ibm plex mono font family
    
    * style(tw): extend tailwind font family
    
    * chore(deps): add iconoir
    
    * feat(wip): add landing page
    
    * chore: apply navbar to layout
    
    * feat: add stars particles
    
    * style: set padding to vertical only
    
    * feat: add footer section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,18 @@ import Link from "next/link"
 import { NAV_ITEMS } from "@/components/NavigationBar/itemsList"
 import ButtonNavBar from "@/components/ui/button/ButtonNavBar"
 import ButtonCta from "@/components/ui/button/ButtonCta"
+import {
+	Phone,
+	Mail,
+	Instagram,
+	TikTok,
+	LinkedIn,
+	Facebook,
+	Twitter,
+	YouTube,
+	Dribbble,
+} from "iconoir-react"
+
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -50,10 +62,10 @@ export default function RootLayout({
 					<NavBar>
 						<Link href="#" passHref>
 							<Image
-								src="/team37-logo.svg"
-								alt="logo"
-								width={128}
-								height={128}
+								src="team37-logo.svg"
+								alt="Team 37 logo"
+								width={150}
+								height={150}
 							/>
 						</Link>
 						<NavItemList>
@@ -71,7 +83,113 @@ export default function RootLayout({
 					</NavBar>
 				</header>
 				{children}
-				<footer></footer>
+				<footer className="flex flex-row justify-between pb-6">
+					<section className="brand-identity flex flex-col justify-between items-start gap-4 h-48 text-base leading-6 font-medium text-zinc-300">
+						<Link href="#">
+							<Image
+								src="team37-logo.svg"
+								alt="Team 37 logo"
+								width={150}
+								height={150}
+							/>
+						</Link>
+						<p>PT Nusa Tekno Solusi</p>
+						<p className="w-[277px]">
+							Ruko Mega Grosir Cempaka Mas Blok I No.51, Jalan
+							Sumur Batu, Kecamatan Kemayoran, Jakarta Pusat
+						</p>
+					</section>
+					<section className="flex flex-col gap-6">
+						<h3 className="text-lg leading-7 font-semibold text-white">
+							Contact Info
+						</h3>
+						<div className="flex flex-col gap-4 text-zinc-300">
+							<Link
+								className="hover:text-zinc-50 transition duration-300"
+								href="mailto:team37.id@gmail.com"
+							>
+								<div className="email flex flex-row gap-2 text-base leading-6 font-normal">
+									<Mail />
+									<p>team37.id@gmail.com</p>
+								</div>
+							</Link>
+							<Link
+								className="hover:text-zinc-50 transition duration-300"
+								href="https://api.whatsapp.com/send?phone=62812878722257"
+							>
+								<div className="phone flex flex-row gap-2">
+									<Phone />
+									<p>+62 812-8787-2257</p>
+								</div>
+							</Link>
+						</div>
+					</section>
+					<section className="flex flex-col gap-6">
+						<h3 className="text-lg leading-7 font-semibold text-white">
+							Sitelinks
+						</h3>
+						<ul className="flex flex-col gap-4 text-zinc-300">
+							<Link
+								className="hover:text-zinc-50 transition duration-300"
+								href="/about"
+							>
+								<li>About</li>
+							</Link>
+							<Link
+								className="hover:text-zinc-50 transition duration-300"
+								href="/services"
+							>
+								<li>Services</li>
+							</Link>
+							<Link
+								className="hover:text-zinc-50 transition duration-300"
+								href="/projects"
+							>
+								<li>Projects</li>
+							</Link>
+							<Link
+								className="hover:text-zinc-50 transition duration-300"
+								href="/blog"
+							>
+								<li>Blog</li>
+							</Link>
+							<Link
+								className="hover:text-zinc-50 transition duration-300"
+								href="/careers"
+							>
+								<li>Careers</li>
+							</Link>
+						</ul>
+					</section>
+					<section className="flex flex-col gap-6">
+						<h3 className="text-lg leading-7 font-semibold text-white">
+							Follow Us
+						</h3>
+						<div className="grid grid-cols-4 flex-row gap-4 text-zinc-300">
+							<Link href="https://instagram.com/team37.id">
+								<Instagram />
+							</Link>
+							<Link href="">
+								<TikTok />
+							</Link>
+							<Link href="https://www.linkedin.com/company/team-37-id/">
+								<LinkedIn />
+							</Link>
+							<Link href="">
+								<Facebook />
+							</Link>
+							<Link href="">
+								<Twitter />
+							</Link>
+							<Link href="">
+								<YouTube />
+							</Link>
+							<Link href="">
+								<Dribbble />
+							</Link>
+						</div>
+					</section>
+				</footer>
 			</body>
 		</html>
 	)
