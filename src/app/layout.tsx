1d557fb7026f934c0992c68bee5037483a42dc65
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-07-25 16:52:47 +0700

    chore: apply navbar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import Script from "next/script"
 import "./globals.css"
 import { Inter } from "next/font/google"
+import NavBar from "@/components/NavigationBar/NavBar"
+import NavItemList from "@/components/NavigationBar/NavItemList"
+import Image from "next/image"
+import Link from "next/link"
+import { NAV_ITEMS } from "@/components/NavigationBar/itemsList"
+import ButtonNavBar from "@/components/ui/button/ButtonNavBar"
+import ButtonCta from "@/components/ui/button/ButtonCta"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -36,7 +43,36 @@ export default function RootLayout({
 					}}
 				/>
 			</head>
-			<body className={inter.className}>{children}</body>
+			<body
+				className={`${inter.className} flex flex-col justify-between max-w-7xl md:min-w-3xl mx-auto top-0 bg-black`}
+			>
+				<header>
+					<NavBar>
+						<Link href="#" passHref>
+							<Image
+								src="/team37-logo.svg"
+								alt="logo"
+								width={128}
+								height={128}
+							/>
+						</Link>
+						<NavItemList>
+							{NAV_ITEMS.map((item) => (
+								<Link
+									href={item.href || "/"}
+									key={item.label}
+									target="_blank"
+								>
+									<ButtonNavBar>{item.label}</ButtonNavBar>
+								</Link>
+							))}
+						</NavItemList>
+						<ButtonCta>Contact us</ButtonCta>
+					</NavBar>
+				</header>
+				{children}
+				<footer></footer>
+			</body>
 		</html>
 	)
 }
