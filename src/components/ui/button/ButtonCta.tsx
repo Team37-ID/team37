dc2c7de467e0cda5fe3c21a4bbfa93b2e8de98fc
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-07-25 12:38:42 +0700

    style: change padding to p-1

--- src/components/ui/button/ButtonCta.tsx
+++ src/components/ui/button/ButtonCta.tsx
@@ -7,7 +7,7 @@ interface Props {
 const ButtonCta = ({ children }: Props) => {
 	return (
 		<div className="relative">
-			<div className="inset-0 bg-gradient-to-r p-[2px] mx-auto from-blue-500 to-[#8B5CF6] rounded-lg">
+			<div className="inset-0 bg-gradient-to-r p-1 mx-auto from-blue-500 to-[#8B5CF6] rounded-xl">
 				<button className="bg-zinc-800 px-4 py-2 rounded-lg inline-flex justify-center items-center font-medium text-lg leading-7 transition duration-300 drop-shadow-glow-sm hover:transition hover:duration-300 hover:drop-shadow-glow-lg hover:bg-zinc-900">
 					{children}
 				</button>
