71009c822b4584602464fa54d0a6e883c5a80158
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-07-26 21:09:22 +0700

    feat: add projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import ButtonCta from "@/components/ui/button/ButtonCta"
 import Image from "next/image"
+import Link from "next/link"
+import ButtonNormal from "@/components/ui/button/ButtonNormal"
+import { ArrowRight } from "iconoir-react"
 
 export default function Home() {
 	return (
-		<main className="flex flex-col items-center my-52 justify-between min-h-screen">
-			<section className="landing-page flex flex-col items-center justify-center">
+		<main className="flex flex-col justify-between min-h-screen gap-64 my-52">
+			<section className="flex flex-col items-center justify-center landing-page">
 				<Image
 					className="absolute z-10 hidden md:block"
 					src="aurora.svg"
@@ -14,8 +17,8 @@ export default function Home() {
 				/>
 				<div className="flex flex-col gap-16">
 					<div className="flex flex-col gap-12">
-						<div className="hero-headline z-20">
-							<h1 className="md:text-7xl md:leading-none text-center font-bold">
+						<div className="z-20 hero-headline">
+							<h1 className="font-bold text-center md:text-7xl md:leading-none">
 								<span className="bg-gradient-to-br bg-clip-text from-white to-[#706F6C] text-transparent">
 									Scaling New Horizons
 									<br />
@@ -23,7 +26,7 @@ export default function Home() {
 								</span>
 								<br />
 							</h1>
-							<h1 className="flex flex-row items-center justify-center gap-4 md:text-7xl text-center md:leading-none font-bold">
+							<h1 className="flex flex-row items-center justify-center gap-4 font-bold text-center md:text-7xl md:leading-none">
 								<span className="text-[#2CDA9D] w-[137px]">
 									Dev
 								</span>
@@ -32,14 +35,14 @@ export default function Home() {
 								</span>
 							</h1>
 						</div>
-						<div className="hero-subheadline z-20">
-							<h2 className="md:text-2xl md:leading-8 font-medium text-white">
+						<div className="z-20 hero-subheadline">
+							<h2 className="font-medium text-white md:text-2xl md:leading-8">
 								We aim to assist your business in advancing
 								through technology.
 							</h2>
 						</div>
 					</div>
-					<div className="flex flex-row items-center z-20 justify-center gap-2 text-lg leading-7 font-medium">
+					<div className="z-20 flex flex-row items-center justify-center gap-2 text-lg font-medium leading-7">
 						<span>Press</span>
 						<span className="flex items-center font-ibm-plexmono justify-center w-8 h-8 bg-gradient-to-br from-white to-[#706F6C] text-stone-700 rounded-md">
 							C
@@ -48,8 +51,81 @@ export default function Home() {
 					</div>
 				</div>
 			</section>
-			<section className="banner relative border-transparent w-full bg-zinc-800 rounded-2xl">
-				<div className="flex flex-col w-full justify-center align-middle items-center py-12 gap-12 z-30">
+			<section className="flex flex-col items-start gap-8 projects">
+				<h1 className="font-medium text-white uppercase md:text-xl md:leading-7">
+					Projects
+				</h1>
+				<div className="flex flex-col gap-32">
+					<div className="flex flex-col tob-brands">
+						<div className="flex flex-col gap-16">
+							<div className="flex flex-col gap-6">
+								<h2 className="md:text-4xl md:leading-10 font-bold bg-gradient-to-r bg-clip-text text-transparent from-blue-500 to-[#8B5CF6]">
+									Trusted by Top Brands Worldwide
+								</h2>
+								<p className="md:text-xl md:leading-7 font-normal text-zinc-400 w-[563px]">
+									Empowering businesses globally with our
+									proven expertise. Your success starts here.
+								</p>
+							</div>
+							<Link
+								href="https://www.cozylila.com"
+								target="_blank"
+								passHref
+							>
+								<Image
+									src="cozylila.svg"
+									width={150}
+									height={64}
+									alt="Cozylila logo"
+								/>
+							</Link>
+						</div>
+					</div>
+					<div className="flex flex-col our-projects">
+						<div className="flex flex-col gap-16">
+							<div className="flex flex-col gap-6">
+								<h2 className="md:text-4xl md:leading-10 font-bold bg-gradient-to-r bg-clip-text text-transparent from-blue-500 to-[#8B5CF6]">
+									Discover Our Projects
+								</h2>
+								<p className="md:text-xl md:leading-7 font-normal text-zinc-400 w-[563px]">
+									Explore our various inspiring journey that
+									full of dedications to create innovative
+									projects and solutions.
+								</p>
+								<ButtonNormal>
+									<span>Discover our showcase</span>
+									<ArrowRight />
+								</ButtonNormal>
+							</div>
+							<div className="flex flex-col items-center justify-center gap-4 w-max">
+								<Image
+									className="items-center border-2 border-white rounded-2xl"
+									src="/nuxtlabs.png"
+									width={512}
+									height={249}
+									alt="projects"
+								/>
+								<div className="flex flex-col items-center justify-center">
+									<h1 className="font-bold text-white md:text-xl md:leading-7">
+										NuxtLabs: Your vision, today
+									</h1>
+									<Link
+										href="https://nuxtlabs.com"
+										target="_blank"
+										passHref
+									>
+										<span className="text-base font-normal leading-6 text-blue-400 hover:border-b hover:border-blue-400">
+											nuxtlabs.com
+										</span>
+									</Link>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section className="relative w-full border-transparent banner bg-zinc-800 rounded-2xl">
+				<div className="z-30 flex flex-col items-center justify-center w-full gap-12 py-12 align-middle">
 					<Image
 						className="absolute left-2 blur-lg w-fit h-fit"
 						src="abstract.svg"
@@ -58,7 +134,7 @@ export default function Home() {
 						alt="abstract"
 					/>
 					<Image
-						className="absolute opacity-10 w-full h-fit rounded-2xl"
+						className="absolute w-full opacity-10 h-fit rounded-2xl"
 						src="noise.svg"
 						width={2048}
 						height={2048}
