8ec8c724b2c63d2b6e413f0e3d7989eef9ff2ae9
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-07-29 21:47:02 +0700

    feat: add team 37 services card variations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,15 @@ import ButtonCta from "@/components/ui/button/ButtonCta"
 import Image from "next/image"
 import Link from "next/link"
 import ButtonNormal from "@/components/ui/button/ButtonNormal"
-import { ArrowRight, Code } from "iconoir-react"
+import {
+	ArrowRight,
+	Code,
+	DesignPencil,
+	PcFirewall,
+	Community,
+	GraduationCap,
+	Brain,
+} from "iconoir-react"
 import { useState } from "react"
 
 export default function Home() {
@@ -233,10 +241,10 @@ export default function Home() {
 				</div>
 			</section>
 			<section className="flex flex-col items-start gap-8 testimonials">
-				<h1 className="font-medium text-white uppercase md:text-xl md:leading-7">
+				<h1 className="font-medium text-white w-full uppercase md:text-xl md:leading-7">
 					Services
 				</h1>
-				<div className="flex flex-col gap-32">
+				<div className="flex flex-col gap-32 w-full">
 					<div className="flex flex-col tob-brands">
 						<div className="flex flex-col gap-16">
 							<div className="flex flex-col gap-6">
@@ -253,8 +261,7 @@ export default function Home() {
 									<ArrowRight />
 								</ButtonNormal>
 							</div>
-							{/* TODO: Add service card here */}
-							<div className="grid grid-rows-3 items-center justify-between align-middle">
+							<div className="grid md:grid-cols-3 grid-flow-row items-center justify-between gap-4 align-middle">
 								<div className="group w-[446px] overflow-hidden h-52 bg-neutral-900 transition-all duration-300 hover:bg-dev-card rounded-lg border px-6 border-neutral-400">
 									<div className="flex flex-col justify-between align-middle w-full">
 										<div className="flex flex-col items-end">
@@ -278,9 +285,159 @@ export default function Home() {
 												height={29}
 												alt="37.dev logo"
 											/>
-											<p className="md:text-base md:leading-6 font-medium text-zinc-300">
+											<p className="w-64 md:text-base md:leading-6 font-medium text-zinc-300">
 												Dream big, leave the details to
 												us
+											</p>
+										</div>
+									</div>
+								</div>
+								<div className="group w-[446px] overflow-hidden h-52 bg-neutral-900 transition-all duration-300 hover:bg-des-card rounded-lg border px-6 border-neutral-400">
+									<div className="flex flex-col justify-between align-middle w-full">
+										<div className="flex flex-col items-end">
+											<div className="text-transparent">
+												abc
+											</div>
+											<div className="duration-300">
+												<div className="w-[90px] h-[90px] bg-des-service rounded-full absolute z-10 blur-2xl hidden group-hover:block"></div>
+												<DesignPencil
+													fontWeight={6}
+													width={96}
+													height={96}
+													className="z-20 text-zinc-800 mr-6 group-hover:text-des-service relative bg-transparent"
+												/>
+											</div>
+										</div>
+										<div className="flex flex-col gap-4 transform translate-y-10 group-hover:-translate-y-4 duration-300">
+											<Image
+												src="37.des.svg"
+												width={128}
+												height={29}
+												alt="37.des logo"
+											/>
+											<p className="w-64 md:text-base md:leading-6 font-medium text-zinc-300">
+												Express your creative sides and
+												let us do the work
+											</p>
+										</div>
+									</div>
+								</div>
+								<div className="group w-[446px] overflow-hidden h-52 bg-neutral-900 transition-all duration-300 hover:bg-sec-card rounded-lg border px-6 border-neutral-400">
+									<div className="flex flex-col justify-between align-middle w-full">
+										<div className="flex flex-col items-end">
+											<div className="text-transparent">
+												abc
+											</div>
+											<div className="duration-300">
+												<div className="w-[90px] h-[90px] bg-sec-service rounded-full absolute z-10 blur-2xl hidden group-hover:block"></div>
+												<PcFirewall
+													fontWeight={6}
+													width={96}
+													height={96}
+													className="z-20 text-zinc-800 mr-6 group-hover:text-sec-service relative bg-transparent"
+												/>
+											</div>
+										</div>
+										<div className="flex flex-col gap-4 transform translate-y-10 group-hover:-translate-y-4 duration-300">
+											<Image
+												src="37.sec.svg"
+												width={128}
+												height={29}
+												alt="37.sec logo"
+											/>
+											<p className="w-64 md:text-base md:leading-6 font-medium text-zinc-300">
+												Keeping you one step ahead from
+												the bad actor
+											</p>
+										</div>
+									</div>
+								</div>
+								<div className="group w-[446px] overflow-hidden h-52 bg-neutral-900 transition-all duration-300 hover:bg-out-card rounded-lg border px-6 border-neutral-400">
+									<div className="flex flex-col justify-between align-middle w-full">
+										<div className="flex flex-col items-end">
+											<div className="text-transparent">
+												abc
+											</div>
+											<div className="duration-300">
+												<div className="w-[90px] h-[90px] bg-out-service rounded-full absolute z-10 blur-2xl hidden group-hover:block"></div>
+												<Community
+													fontWeight={6}
+													width={96}
+													height={96}
+													className="z-20 text-zinc-800 mr-6 group-hover:text-out-service relative bg-transparent"
+												/>
+											</div>
+										</div>
+										<div className="flex flex-col gap-4 transform translate-y-10 group-hover:-translate-y-4 duration-300">
+											<Image
+												src="37.out.svg"
+												width={128}
+												height={29}
+												alt="37.out logo"
+											/>
+											<p className="w-64 md:text-base md:leading-6 font-medium text-zinc-300">
+												Leveraging external expertise
+												for optimal performance
+											</p>
+										</div>
+									</div>
+								</div>
+								<div className="group w-[446px] overflow-hidden h-52 bg-neutral-900 transition-all duration-300 hover:bg-edu-card rounded-lg border px-6 border-neutral-400">
+									<div className="flex flex-col justify-between align-middle w-full">
+										<div className="flex flex-col items-end">
+											<div className="text-transparent">
+												abc
+											</div>
+											<div className="duration-300">
+												<div className="w-[90px] h-[90px] bg-edu-service rounded-full absolute z-10 blur-2xl hidden group-hover:block"></div>
+												<GraduationCap
+													fontWeight={6}
+													width={96}
+													height={96}
+													className="z-20 text-zinc-800 mr-6 group-hover:text-edu-service relative bg-transparent"
+												/>
+											</div>
+										</div>
+										<div className="flex flex-col gap-4 transform translate-y-10 group-hover:-translate-y-4 duration-300">
+											<Image
+												src="37.edu.svg"
+												width={128}
+												height={29}
+												alt="37.edu logo"
+											/>
+											<p className="w-64 md:text-base md:leading-6 font-medium text-zinc-300">
+												Freshen your knowledge to stay
+												competitive in the industry
+											</p>
+										</div>
+									</div>
+								</div>
+								<div className="group w-[446px] overflow-hidden h-52 bg-neutral-900 transition-all duration-300 hover:bg-ai-card rounded-lg border px-6 border-neutral-400">
+									<div className="flex flex-col justify-between align-middle w-full">
+										<div className="flex flex-col items-end">
+											<div className="text-transparent">
+												abc
+											</div>
+											<div className="duration-300">
+												<div className="w-[90px] h-[90px] bg-ai-service rounded-full absolute z-10 blur-2xl hidden group-hover:block"></div>
+												<Brain
+													fontWeight={6}
+													width={96}
+													height={96}
+													className="z-20 text-zinc-800 mr-6 group-hover:text-ai-service relative bg-transparent"
+												/>
+											</div>
+										</div>
+										<div className="flex flex-col gap-4 transform translate-y-10 group-hover:-translate-y-4 duration-300">
+											<Image
+												src="37.ai.svg"
+												width={100}
+												height={29}
+												alt="37.ai logo"
+											/>
+											<p className="w-64 md:text-base md:leading-6 font-medium text-zinc-300">
+												Transform your workflow with the
+												help of AI
 											</p>
 										</div>
 									</div>
