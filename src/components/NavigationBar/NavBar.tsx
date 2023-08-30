b2185a7eb380e9b5a97a9ad30b738854b36efa7f
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-08-30 21:38:38 +0700

    refactor: migrate component to use next ui

--- src/components/NavigationBar/NavBar.tsx
+++ src/components/NavigationBar/NavBar.tsx
@@ -1,42 +1,61 @@
 "use client"
 
-import React, { useState, useEffect } from "react"
+import {
+	Navbar,
+	NavbarBrand,
+	NavbarContent,
+	NavbarItem,
+	NavbarMenu,
+	NavbarMenuItem,
+	NavbarMenuToggle,
+} from "@nextui-org/react"
+import React, { useState } from "react"
+import { NAV_ITEMS } from "./itemsList"
+import Team37Logo from "@/components/logo/Team37Logo"
+import ButtonNavBar from "@/components/ui/button/ButtonNavBar"
+import ButtonCta from "@/components/ui/button/ButtonCta"
+import Link from "next/link"
 
-interface NavBarProps {
-	children: React.ReactNode
-}
-
-const NavBar = ({ children }: NavBarProps) => {
-	const [scrollBackground, setScrollBackground] = useState("bg-transparent")
-
-	useEffect(() => {
-		const handleScroll = () => {
-			const scrollPosition = window.scrollY
-
-			const threshold = 100
-
-			if (scrollPosition > threshold) {
-				setScrollBackground(
-					"bg-gradient-to-br from-glassmorphism-38 to-glassmorphism-08 stroke-[#202020] backdrop-blur-xl z-50"
-				)
-			} else {
-				setScrollBackground("bg-transparent")
-			}
-		}
-
-		window.addEventListener("scroll", handleScroll)
-
-		return () => {
-			window.removeEventListener("scroll", handleScroll)
-		}
-	}, [])
+const NavBar = () => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false)
 
 	return (
-		<nav
-			className={`fixed w-full max-w-8xl top-0 flex flex-row items-center justify-between align-middle z-50 py-6 bg-transparent border-b-[1px] border-b-[#202020] ${scrollBackground}`}
+		<Navbar
+			isBordered
+			maxWidth="2xl"
+			className="fixed bg-gradient-to-br from-glassmorphism-38 to-glassmorphism-08 stroke-[#202020] backdrop-blur-xl z-50 py-2 "
 		>
-			{children}
-		</nav>
+			<NavbarContent>
+				<NavbarMenuToggle
+					aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+					className="sm:hidden"
+				/>
+				<NavbarBrand>
+					<Team37Logo width={150} height={150} />
+				</NavbarBrand>
+			</NavbarContent>
+			<NavbarContent className="hidden sm:flex gap-4">
+				{NAV_ITEMS.map((item, index) => (
+					<NavbarItem key={`${item}-${index}`}>
+						<ButtonNavBar>
+							<Link href={`${item.href}`}>{item.label}</Link>
+						</ButtonNavBar>
+					</NavbarItem>
+				))}
+			</NavbarContent>
+			<NavbarContent justify="end">
+				<ButtonCta>Contact us</ButtonCta>
+			</NavbarContent>
+			<NavbarMenu className="z-50">
+				{NAV_ITEMS.map((item, index) => (
+					<NavbarMenuItem key={`${item}-${index}`}>
+						<ButtonNavBar>
+							<Link href={`${item.href}`}>{item.label}</Link>
+						</ButtonNavBar>
+					</NavbarMenuItem>
+				))}
+			</NavbarMenu>
+		</Navbar>
 	)
 }
 
