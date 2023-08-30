a7a256a0d069e2ec2989f2bd9d9f40386381efa3
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-08-30 21:26:59 +0700

    style: adjust text size for mobile view

--- src/components/ui/button/ButtonCta.tsx
+++ src/components/ui/button/ButtonCta.tsx
@@ -8,7 +8,7 @@ const ButtonCta = ({ children }: Props) => {
 	return (
 		<div className="relative">
 			<div className="inset-0 bg-gradient-to-r p-1 mx-auto from-blue-500 to-[#8B5CF6] rounded-xl">
-				<button className="bg-zinc-800 px-4 py-2 rounded-lg inline-flex justify-center items-center font-medium text-lg leading-7 transition duration-300 drop-shadow-glow-sm hover:transition hover:duration-300 hover:drop-shadow-glow-lg hover:bg-zinc-900">
+				<button className="bg-zinc-800 px-4 py-2 rounded-lg inline-flex justify-center items-center font-medium text-base leading-4 md:text-lg md:leading-7 transition duration-300 drop-shadow-glow-sm hover:transition hover:duration-300 hover:drop-shadow-glow-lg hover:bg-zinc-900">
 					{children}
 				</button>
 			</div>
