6c5120156d148d5f992ffa5d16bc7dca813bb5ab
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-08-31 23:28:13 +0700

    refactor: use dynamic item for footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,19 +3,10 @@
 import React from "react"
 import Link from "next/link"
 import Team37Logo from "../logo/Team37Logo"
-import {
-	Phone,
-	Mail,
-	Instagram,
-	TikTok,
-	LinkedIn,
-	Facebook,
-	Twitter,
-	YouTube,
-	Dribbble,
-	GitHub,
-} from "iconoir-react"
 import { NAV_ITEMS } from "../NavigationBar/itemsList"
+import { SOCIAL_ITEMS_LIST } from "./socialItemsList"
+import { CONTACT_ITEMS_LIST } from "./contactItemsList"
+import { Link as LinkNextUi } from "@nextui-org/react"
 
 const Footer = () => {
 	return (
@@ -35,26 +26,19 @@ const Footer = () => {
 					<h3 className="text-lg leading-7 font-semibold text-white">
 						Contact Info
 					</h3>
-					<div className="flex flex-col gap-4 text-zinc-300">
+					{CONTACT_ITEMS_LIST.map((item, index) => (
 						<Link
+							key={`${item}-${index}`}
 							className="hover:text-zinc-50 transition duration-300"
-							href="mailto:team37.id@gmail.com"
+							href={`${item.href}`}
+							target={item.target}
 						>
-							<div className="email flex flex-row gap-4 text-base leading-6 font-normal">
-								<Mail />
-								<p>team37.id@gmail.com</p>
+							<div className="flex flex-row gap-4 text-base leading-6 font-normal">
+								<item.icon />
+								<p>{item.label}</p>
 							</div>
 						</Link>
-						<Link
-							className="hover:text-zinc-50 transition duration-300"
-							href="https://api.whatsapp.com/send?phone=62812878722257"
-						>
-							<div className="phone flex flex-row gap-4">
-								<Phone />
-								<p>+62 812-8787-2257</p>
-							</div>
-						</Link>
-					</div>
+					))}
 				</section>
 				<section className="flex flex-col gap-6">
 					<h3 className="text-lg leading-7 font-semibold text-white">
@@ -76,59 +60,18 @@ const Footer = () => {
 					<h3 className="text-lg leading-7 font-semibold text-white">
 						Follow Us
 					</h3>
-					<div className="flex flex-col gap-6 md:gap-4 text-zinc-300">
-						<div className="flex flex-row gap-4 md:gap-6">
-							<Link
-								className="hover:text-zinc-500 transition duration-100 ease-linear"
-								href="https://instagram.com/team37.id"
+					<div className="flex md:flex-row flex-wrap w-44 gap-4 md:gap-6">
+						{SOCIAL_ITEMS_LIST.map((item, index) => (
+							<LinkNextUi
+								key={`${item}-${index}`}
+								className="text-white hover:text-zinc-500 transition duration-100 ease-linear"
+								href={`${item.href}`}
+								target={item.target}
+								isDisabled={item.isDisabled}
 							>
-								<Instagram />
-							</Link>
-							<Link
-								className="hover:text-zinc-500 transition duration-100 ease-linear"
-								href=""
-							>
-								<TikTok />
-							</Link>
-							<Link
-								className="hover:text-zinc-500 transition duration-100 ease-linear"
-								href="https://www.linkedin.com/company/team-37-id/"
-							>
-								<LinkedIn />
-							</Link>
-							<Link
-								className="hover:text-zinc-500 transition duration-100 ease-linear"
-								href=""
-							>
-								<Facebook />
-							</Link>
-						</div>
-						<div className="flex flex-row gap-4 md:gap-6">
-							<Link
-								className="hover:text-zinc-500 transition duration-100 ease-linear"
-								href=""
-							>
-								<Twitter />
-							</Link>
-							<Link
-								className="hover:text-zinc-500 transition duration-100 ease-linear"
-								href=""
-							>
-								<YouTube />
-							</Link>
-							<Link
-								className="hover:text-zinc-500 transition duration-100 ease-linear"
-								href=""
-							>
-								<Dribbble />
-							</Link>
-							<Link
-								className="hover:text-zinc-500 transition duration-100 ease-linear"
-								href="https://github.com/Team37-ID"
-							>
-								<GitHub />
-							</Link>
-						</div>
+								<item.icon />
+							</LinkNextUi>
+						))}
 					</div>
 				</section>
 			</div>
