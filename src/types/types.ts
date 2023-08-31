42065da34305d6d41746a5f9dae7ac76ebcef21a
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-08-31 23:27:42 +0700

    feat: add contact and social media types

--- src/types/types.ts
+++ src/types/types.ts
@@ -4,3 +4,18 @@ export type NavTypes = {
 	href?: string
 	target?: string
 }
+
+export type ContactTypes = {
+	label: string
+	href?: string
+	target?: string
+	icon: any
+	isDisabled?: boolean
+}
+
+export type SocialMediaTypes = {
+	href?: string
+	target?: string
+	icon: any
+	isDisabled?: boolean
+}
