41c5346f7679a993a89cf7cf5c79df9b80b7d124
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-09-01 21:17:13 +0700

    refactor: apply jargon component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ import LandingPage from "@/components/content/home/LandingPage"
 import Projects from "@/components/content/home/team37-project/Projects"
 import Testimonials from "@/components/content/home/team37-testimonials/Testimonials"
 import Services from "@/components/content/home/team37-services/Services"
+import Jargon from "@/components/content/home/Jargon"
 
 export default function Home() {
 	const mobileBreakpoint = 763
@@ -52,35 +53,7 @@ export default function Home() {
 				<Services />
 			</section>
 			<section className="relative border-transparent banner bg-zinc-800 rounded-2xl overflow-hidden">
-				<div className="z-30 flex flex-col items-center justify-center gap-12 py-12 align-middle">
-					<Image
-						className="absolute left-2 blur-lg w-fit h-fit"
-						src="abstract.svg"
-						width={128}
-						height={128}
-						alt="abstract"
-					/>
-					<Image
-						className="absolute object-cover top-0 left-0 opacity-10 w-full h-full"
-						src="noise.svg"
-						width={2048}
-						height={2048}
-						alt="abstract"
-					/>
-					<Image
-						className="z-30"
-						src="team37-logo.svg"
-						alt="Team 37 logo"
-						width={isMobileView ? 99 : 150}
-						height={isMobileView ? 16 : 150}
-					/>
-					<span className="flex flex-col z-30 align-middle items-center justify-center text-5xl md:text-7xl leading-none font-bold bg-gradient-to-br bg-clip-text text-transparent from-white to-[#706F6C]">
-						<h1>We listen</h1>
-						<h1>We do</h1>
-						<h1>We deliver</h1>
-					</span>
-					<ButtonCta>Free consultation</ButtonCta>
-				</div>
+				<Jargon />
 			</section>
 		</main>
 	)
