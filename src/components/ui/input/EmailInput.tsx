a12773107f1d1650f94c85ed8b6464032c67b381
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-09-02 20:01:50 +0700

    feat: add email input component

---
+++ src/components/ui/input/EmailInput.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { Input } from "@nextui-org/react"
+import { clear } from "console"
+import { useMemo, useState } from "react"
+
+const EmailInput = () => {
+	const [value, setValue] = useState("")
+
+	const validationState = useMemo(() => {
+		const validateEmail = (val: string) =>
+			value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i)
+		if (value === "") {
+			return undefined
+		}
+
+		return validateEmail(value) ? "valid" : "invalid"
+	}, [value])
+
+	return (
+		<>
+			<Input
+				type="email"
+				isRequired
+				variant="underlined"
+				placeholder="johndoe@example.com"
+				label="Email"
+				description="We'll never share your email with anyone else."
+				color={validationState === "invalid" ? "danger" : "success"}
+				errorMessage={
+					validationState === "invalid" &&
+					"Please enter a valid email"
+				}
+				validationState={validationState}
+				value={value}
+				onValueChange={setValue}
+			/>
+		</>
+	)
+}
+
+export default EmailInput
