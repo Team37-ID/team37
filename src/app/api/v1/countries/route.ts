18d47b329181c6b1b275d2fa3b0fcb8e32c909f3
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-09-07 22:35:59 +0700

    chore: update country api

--- src/app/api/v1/countries/route.ts
+++ src/app/api/v1/countries/route.ts
@@ -3,7 +3,35 @@ import NextSupabase from "@/app/supabase/supabase"
 
 export async function GET(request: NextResponse) {
 	try {
-		const { data, error } = await NextSupabase.from("country").select("*")
+		const totalCountry = 241
+		const { searchParams } = new URL(request.url)
+		let limit = Number(searchParams.get("limit")) || 20
+		let offset = Number(searchParams.get("offset")) || 0
+
+		// Validate and set default values for limit and offset
+		if (Number.isNaN(limit) || limit <= 0) {
+			limit = 20
+		}
+
+		if (Number.isNaN(offset) || offset < 0) {
+			offset = 0
+		} else {
+			offset = Math.min(offset, 241)
+		}
+
+		const nextOffset = offset + limit
+		const prevOffset = offset - limit
+		const next =
+			nextOffset > totalCountry
+				? null
+				: `https://team37.co/api/v1/countries?offset=${nextOffset}&limit=${limit}`
+		let previous =
+			offset === 0
+				? null
+				: `https://team37.co/api/v1/countries?offset=${prevOffset}&limit=${limit}`
+		const { data, error } = await NextSupabase.from("country")
+			.select("*")
+			.range(limit as number, (offset as number) - 1)
 		const length = data?.length
 
 		if (error) {
@@ -20,7 +48,13 @@ export async function GET(request: NextResponse) {
 			})
 		}
 
-		return NextResponse.json({ length, data })
+		return NextResponse.json({
+			totalCountry,
+			length,
+			next,
+			previous,
+			data,
+		})
 	} catch (err) {
 		console.log("Internal server error: ", err)
 		return NextResponse.json({ error: "An internal server error occured." })
