f2ad26b5928a05de86c489ea922bf190601b76d8
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-09-07 18:35:16 +0700

    feat: add countries api

---
+++ src/app/api/v1/countries/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server"
+import NextSupabase from "@/app/supabase/supabase"
+
+export async function GET(request: NextResponse) {
+	try {
+		const { data, error } = await NextSupabase.from("country").select("id")
+
+		if (error) {
+			console.log("Supabase error: ", error.message)
+			return NextResponse.json({
+				error: "An error occured while fetching data",
+			})
+		}
+
+		if (!data) {
+			console.log("No data found in Supabase")
+			return NextResponse.json({
+				error: "No data available00000",
+			})
+		}
+
+		return NextResponse.json(data)
+	} catch (err) {
+		console.log("Internal server error: ", err)
+		return NextResponse.json({ error: "An internal server error occured." })
+	}
+}
