cc2561abf33363fdcfcd06337e786538d0c2a860
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-09-08 19:40:38 +0700

    fix: set the correct api response

--- src/app/api/v1/countries/route.ts
+++ src/app/api/v1/countries/route.ts
@@ -1,55 +1,71 @@
 import { NextResponse } from "next/server"
 import NextSupabase from "@/app/supabase/supabase"
 
+interface Props {
+	offset: number
+	limit: number
+}
+
+const MAX_OFFSET_LIMIT = 241
+
+function isValidOffsetAndLimit({ offset, limit }: Props) {
+	return (
+		typeof offset === "number" &&
+		typeof limit === "number" &&
+		offset >= 0 &&
+		limit >= 0 &&
+		offset <= MAX_OFFSET_LIMIT &&
+		limit <= MAX_OFFSET_LIMIT
+	)
+}
+
+function generateURLWithOffsetAndLimit({ offset, limit }: Props) {
+	return `https://team37.co/api/v1/countries?offset=${offset}&limit=${limit}`
+}
+
 export async function GET(request: NextResponse) {
 	try {
-		const totalCountry = 241
+		const totalCountry = MAX_OFFSET_LIMIT
 		const { searchParams } = new URL(request.url)
+
 		let limit = Number(searchParams.get("limit")) || 20
 		let offset = Number(searchParams.get("offset")) || 0
 
-		// Validate and set default values for limit and offset
-		if (Number.isNaN(limit) || limit <= 0) {
-			limit = 20
-		}
-
-		if (Number.isNaN(offset) || offset < 0) {
-			offset = 0
-		} else {
-			offset = Math.min(offset, 241)
-		}
-
-		if (limit === offset) {
-			limit = 0
-		}
-
 		const nextOffset = offset + limit
-		const prevOffset = offset - limit
+		const previousOffset = offset - limit
+
+		const from = Math.min(limit, offset)
+		const to = Math.max(limit, nextOffset)
+
+		const isValidRequest = isValidOffsetAndLimit({ offset, limit })
 		const next =
-			nextOffset > totalCountry
-				? null
-				: `https://team37.co/api/v1/countries?offset=${nextOffset}&limit=${limit}`
-		let previous =
-			offset === 0
-				? null
-				: `https://team37.co/api/v1/countries?offset=${prevOffset}&limit=${limit}`
+			isValidRequest && nextOffset <= MAX_OFFSET_LIMIT
+				? generateURLWithOffsetAndLimit({ offset: nextOffset, limit })
+				: null
+		const previous =
+			isValidRequest && offset >= limit
+				? generateURLWithOffsetAndLimit({
+						offset: previousOffset,
+						limit,
+				  })
+				: null
 
 		const { data, error } = await NextSupabase.from("country")
 			.select("*")
-			.range(limit as number, (offset as number) - 1)
+			.range(from, to - 1)
 		const length = data?.length
 
 		if (error) {
-			console.log("Supabase error: ", error.message)
+			console.error("Supabase error:", error.message)
 			return NextResponse.json({
-				error: "An error occured while fetching data",
+				error: "An error occurred while fetching data",
 			})
 		}
 
 		if (!data) {
-			console.log("No data found in Supabase")
+			console.error("No data found in Supabase")
 			return NextResponse.json({
-				error: "No data available00000",
+				error: "No data available",
 			})
 		}
 
@@ -61,7 +77,9 @@ export async function GET(request: NextResponse) {
 			data,
 		})
 	} catch (err) {
-		console.log("Internal server error: ", err)
-		return NextResponse.json({ error: "An internal server error occured." })
+		console.error("Internal server error:", err)
+		return NextResponse.json({
+			error: "An internal server error occurred.",
+		})
 	}
 }
