2b278e586bf05abe81c7d0982d5a51b2e1f8244b
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-09-08 22:49:29 +0700

    chore: route the href to root

--- src/components/navbar/itemsList.ts
+++ src/components/navbar/itemsList.ts
@@ -1,10 +1,10 @@
 import { NavTypes } from "@/types/types"
 
 export const NAV_ITEMS: NavTypes[] = [
-	{ href: "/about", label: "About" },
-	{ href: "/services", label: "Services" },
-	{ href: "/projects", label: "Projects" },
-	{ href: "/blog", label: "Blog" },
-	{ href: "/careers", label: "Careers" },
-	{ href: "/pricing", label: "Pricing" },
+	{ href: "/", label: "About" },
+	{ href: "/", label: "Services" },
+	{ href: "/", label: "Projects" },
+	{ href: "/", label: "Blog" },
+	{ href: "/", label: "Careers" },
+	{ href: "/", label: "Pricing" },
 ]
