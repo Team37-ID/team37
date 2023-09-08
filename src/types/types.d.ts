9aec9bb985507dd0521fe503c006bad6fa27e8c6
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-09-08 20:19:43 +0700

    feat: add country type and use country list props

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -19,3 +19,14 @@ export type SocialMediaTypes = {
 	icon: any
 	isDisabled?: boolean
 }
+
+export type Country = {
+	id: string
+	alpha2: string
+	country_name: string
+	country_code: string
+}
+
+export type UseCountryListProps = {
+	fetchDelay?: number
+}
