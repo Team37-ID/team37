31551191d21ca5ad9c1948f295f9a4bfe9fcc78f
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-09-08 20:59:06 +0700

    Revert "Push dev to main (#8)"
    This reverts commit 5f59661a48b202b06659460100c1583101f9c3ff.

--- src/app/supabase/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js"
-
-const NextSupabase = createClient(
-	process.env.SUPABASE_URL || "",
-	process.env.SUPABASE_PUBLIC_ANON_KEY || ""
-)
-
-export default NextSupabase
