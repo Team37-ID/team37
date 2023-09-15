b9f2bfef6bed495211954ba18c63641c8f23a616
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-09-15 18:36:38 +0700

    feat: add name input component

---
+++ src/components/ui/input/NameInput.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { Input } from "@nextui-org/react"
+import { useState, ChangeEvent } from "react"
+
+type Props = {
+	placeholder: string
+	label: string
+	desc: string
+	value?: string
+	validationState?: "valid" | "invalid"
+	errorMessage?: string | boolean
+	onChange?: (e: ChangeEvent<HTMLInputElement>) => void
+	onValueChange?: (val: string) => void
+}
+
+const NameInput = ({
+	placeholder,
+	label,
+	desc,
+	value,
+	validationState,
+	errorMessage,
+	onChange,
+	onValueChange,
+}: Props) => {
+	return (
+		<Input
+			type="text"
+			isRequired
+			variant="underlined"
+			placeholder={placeholder}
+			label={label}
+			description={desc}
+			value={value}
+			onValueChange={onValueChange}
+			validationState={validationState}
+			errorMessage={errorMessage}
+			onChange={onChange}
+		/>
+	)
+}
+
+export default NameInput
