e3560ea00de5a4c3afa45951f276ed2aa14b4a5a
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-28 21:49:38 +0700

    Migrate to NextJS 14 (#15)
    * chore: migrate to next.js 14
    
    * chore(deps): add biome
    
    * chore: modify biome formatter config
    
    * chore: add shadcn ui
    
    * remove trailing whitespace
    
    * remove boilerplate
    
    * chore: change project version to 1.1.0
    
    * style: change font to use manrope
    
    * chore: change root metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,57 +1,22 @@
-import Script from "next/script"
+import type { Metadata } from "next"
+import { Manrope } from "next/font/google"
 import "./globals.css"
-import { Inter } from "next/font/google"
-import { Providers } from "./providers"
-import NavBar from "@/components/navbar/NavBar"
-import Footer from "@/components/footer/footer"
 
-const inter = Inter({ subsets: ["latin"] })
+const manrope = Manrope({ subsets: ["latin"] })
 
-export const metadata = {
-	title: "Team 37",
-	description: "Team 37 home page",
+export const metadata: Metadata = {
+  title: "Home | Team 37",
+  description: "Home page of Team 37",
 }
 
 export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode
-}) {
-	return (
-		<html lang="en">
-			<head>
-				<Script
-					async
-					strategy="afterInteractive"
-					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.FIREBASE_MEASUREMENT_ID}`}
-				/>
-				<Script
-					id="ga-id"
-					strategy="afterInteractive"
-					dangerouslySetInnerHTML={{
-						__html: `
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments)}
-						gtag('js', new Date());
-
-						gtag('config', '${process.env.FIREBASE_MEASUREMENT_ID}');
-						`,
-					}}
-				/>
-			</head>
-			<Providers>
-				<body
-					className={`${inter.className} selection:text-blue-900 selection:bg-blue-300 flex flex-col justify-between max-w-9xl md:min-w-3xl md:mx-auto top-0 bg-black`}
-				>
-					<header>
-						<NavBar />
-					</header>
-					{children}
-					<footer className="flex flex-col gap-12 z-40 align-middle pb-6 justify-center mx-7">
-						<Footer />
-					</footer>
-				</body>
-			</Providers>
-		</html>
-	)
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={manrope.className}>{children}</body>
+    </html>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,9 @@
-"use client"
-
-import LandingPage from "@/components/content/home/LandingPage"
-import dynamic from "next/dynamic"
-
-const Projects = dynamic(
-	() => import("@/components/content/home/team37-project/Projects")
-)
-
-const Testimonials = dynamic(
-	() => import("@/components/content/home/team37-testimonials/Testimonials")
-)
-
-const Services = dynamic(
-	() => import("@/components/content/home/team37-services/Services")
-)
-
-const Jargon = dynamic(() => import("@/components/content/home/Jargon"))
+import Image from "next/image"
 
 export default function Home() {
-	return (
-		<main className="flex flex-col justify-between min-h-screen gap-64 my-64 mx-4 md:my-96">
-			<section className="flex flex-col items-center justify-center landing-page">
-				<LandingPage />
-			</section>
-			<section className="flex flex-col items-start gap-8 projects overflow-hidden z-40">
-				<Projects />
-			</section>
-			<section className="flex flex-col items-start gap-8 testimonials z-40">
-				<Testimonials />
-			</section>
-			<section className="flex flex-col items-start gap-8 services z-40">
-				<Services />
-			</section>
-			<section className="relative border-transparent banner bg-zinc-800 rounded-2xl overflow-hidden">
-				<Jargon />
-			</section>
-		</main>
-	)
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>hello world</div>
+    </main>
+  )
 }
