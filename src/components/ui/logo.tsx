d1c623c1a1426e1dacdf57e02babf904a6dbd3bc
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-31 07:06:02 +0700

    feat(comps): add logo component

---
+++ src/components/ui/logo.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import Link from "next/link"
+
+type LogoProps = {
+	width?: number
+	height?: number
+}
+
+const Logo = ({ width, height }: LogoProps) => {
+	return (
+		<div>
+			<Link href="/">
+				<Image
+					src="/logo.svg"
+					alt="Logo"
+					width={width ?? 100}
+					height={height ?? 100}
+				/>
+			</Link>
+		</div>
+	)
+}
+
+export default Logo
