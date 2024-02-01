ea8a2659e3472c24ce5072728c6bbb8f25b3425f
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-02-01 16:56:13 +0700

    feat: add additional sub-menu items to services

--- src/data/navItemsList.ts
+++ src/data/navItemsList.ts
@@ -3,7 +3,44 @@ import { NavItemTypes } from "@/types/types"
 export const navItemsList: NavItemTypes[] = [
 	{
 		title: "Services",
-		subMenu: [{ title: "Development", url: "/development" }],
+		subMenu: [
+			{
+				title: "Development",
+				url: "/development",
+				description: "Lorem ipsum dolor sit amet",
+				icon: "37.dev.svg",
+			},
+			{
+				title: "Design",
+				url: "/development",
+				description: "Lorem ipsum dolor sit amet",
+				icon: "37.des.svg",
+			},
+			// {
+			// 	title: "Security",
+			// 	url: "/development",
+			// 	description: "Lorem ipsum dolor sit amet",
+			// 	icon: "37.sec.svg",
+			// },
+			// {
+			// 	title: "Outsource",
+			// 	url: "/development",
+			// 	description: "Lorem ipsum dolor sit amet",
+			// 	icon: "37.out.svg",
+			// },
+			// {
+			// 	title: "Education",
+			// 	url: "/development",
+			// 	description: "Lorem ipsum dolor sit amet",
+			// 	icon: "37.edu.svg",
+			// },
+			// {
+			// 	title: "AI",
+			// 	url: "/development",
+			// 	description: "Lorem ipsum dolor sit amet",
+			// 	icon: "37.ai.svg",
+			// },
+		],
 		hasSubMenu: true,
 	},
 	{
