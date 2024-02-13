f441f777bf81e0857f3371db5c5fefe98eabb5b8
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-02-13 21:50:16 +0700

    feat: add cta button

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -10,7 +10,6 @@ import {
 	navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 import Logo from "@/components/ui/logo"
-import { Button } from "@/components/ui/button"
 import Link from "next/link"
 import DevService from "./navbar-menu/dev-service"
 import DesService from "./navbar-menu/des-service"
@@ -33,22 +32,30 @@ const Navbar = () => {
 							<DesService />
 						</NavigationMenuContent>
 						<Link href="/projects" passHref>
-							<NavigationMenuLink className={`capitalize ${navigationMenuTriggerStyle()}`}>
+							<NavigationMenuLink
+								className={`capitalize ${navigationMenuTriggerStyle()}`}
+							>
 								Projects
 							</NavigationMenuLink>
 						</Link>
 						<Link href="/pricing" passHref>
-							<NavigationMenuLink className={`capitalize ${navigationMenuTriggerStyle()}`}>
+							<NavigationMenuLink
+								className={`capitalize ${navigationMenuTriggerStyle()}`}
+							>
 								Pricing
 							</NavigationMenuLink>
 						</Link>
 						<Link href="/career" passHref>
-							<NavigationMenuLink className={`capitalize ${navigationMenuTriggerStyle()}`}>
+							<NavigationMenuLink
+								className={`capitalize ${navigationMenuTriggerStyle()}`}
+							>
 								Career
 							</NavigationMenuLink>
 						</Link>
 						<Link href="/blog" passHref>
-							<NavigationMenuLink className={`capitalize ${navigationMenuTriggerStyle()}`}>
+							<NavigationMenuLink
+								className={`capitalize ${navigationMenuTriggerStyle()}`}
+							>
 								Blog
 							</NavigationMenuLink>
 						</Link>
@@ -68,7 +75,15 @@ const Navbar = () => {
 					</NavigationMenuItem>
 				</NavigationMenuList>
 			</NavigationMenu>
-			<Button className="text-white capitalize">Book a call</Button>
+			<button
+				type="button"
+				className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+			>
+				<span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+				<span className="inline-flex h-full w-full px-6 cursor-pointer items-center justify-center rounded-full bg-slate-950 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+					Book A Call
+				</span>
+			</button>
 		</header>
 	)
 }
