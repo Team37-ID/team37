a2c3067fc2fe64d319a6d1186d06cc573b89a734
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-02-16 20:36:35 +0700

    feat: use cta card component to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import CtaCard from "@/components/ui/cta-card"
 import LandingPage from "@/components/ui/landingPage"
 import "@/styles/magicPattern.css"
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center bg-black">
+		<main className="flex min-h-screen flex-col items-center justify-center gap-3.5 bg-black">
 			<LandingPage />
+			<CtaCard />
 		</main>
 	)
 }
