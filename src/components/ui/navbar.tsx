8efd2f25d03809bdd3c2ffe976ef9e906289f134
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-02-16 20:24:50 +0700

    chore(refactor): use cta button component for reusability

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -13,6 +13,7 @@ import Logo from "@/components/ui/logo"
 import Link from "next/link"
 import DevService from "./navbar-menu/dev-service"
 import DesService from "./navbar-menu/des-service"
+import CtaButton from "./cta-button"
 
 const Navbar = () => {
 	return (
@@ -88,15 +89,7 @@ const Navbar = () => {
 					</NavigationMenuItem>
 				</NavigationMenuList>
 			</NavigationMenu>
-			<button
-				type="button"
-				className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
-			>
-				<span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-				<span className="inline-flex h-full w-full px-6 cursor-pointer items-center justify-center rounded-full bg-slate-950 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-					Book A Call
-				</span>
-			</button>
+			<CtaButton />
 		</header>
 	)
 }
