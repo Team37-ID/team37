854f3c9832124758e4327f55c57526ff3fb2dac6
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-03-03 22:00:29 +0700

    feat(comps): wip navigation bar

---
+++ src/components/ui/navigation-bar.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import Logo from "@/components/ui/logo"
+import * as NavigationMenu from "@radix-ui/react-navigation-menu"
+import * as Switch from "@radix-ui/react-switch"
+import { useTheme } from "next-themes"
+
+const NavigationBar = () => {
+	const { theme, setTheme, resolvedTheme } = useTheme()
+
+	return (
+		<>
+			<header className="flex flex-row justify-between bg-white dark:bg-black">
+				<Logo />
+				<Switch.Root
+					checked={resolvedTheme === "dark"}
+					onCheckedChange={(isChecked) =>
+						setTheme(isChecked ? "dark" : "light")
+					}
+					className="w-[42px] h-[25px] bg-blackA6 rounded-full relative shadow-[0_2px_10px] shadow-blackA4 focus:shadow-[0_0_0_2px] focus:shadow-black data-[state=checked]:bg-black outline-none cursor-default"
+				>
+					<Switch.Thumb className="block w-[21px] h-[21px] bg-white rounded-full shadow-[0_2px_2px] shadow-blackA4 transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
+				</Switch.Root>
+			</header>
+		</>
+	)
+}
+
+export default NavigationBar
