a93cafebae7e417acc0355fb812d09cc53f02060
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-03-03 22:00:16 +0700

    feat(page): add hero title and sub-title to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import "@/styles/magicPattern.css"
+import AnimatedText from "@/components/ui/animated-text"
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center gap-3.5 bg-black">
-			avc
+		<main className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-black">
+			<span className="flex flex-col gap-4 md:items-start border-4 w-full">
+				<AnimatedText
+					text={["You", "Found Us!"]}
+					className="md:text-[12rem] text-6xl font-light tracking-tighter capitalize font-chivo-mono"
+				/>
+				<p className="text-4xl font-manrope font-light text-black dark:text-white">
+					We are a one-stop digital solution for all your business
+					needs.
+				</p>
+			</span>
 		</main>
 	)
 }
