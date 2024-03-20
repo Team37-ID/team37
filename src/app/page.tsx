952c5bc8cbe3ff989e039d3466181649a5d3a027
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-03-20 19:00:08 +0700

    chore(wip): add hero content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
+"use client"
+
 import AnimatedText from "@/components/ui/animated-text"
+import { Hero } from "@/components/ui/hero"
+import ShaderBackground from "@/components/ui/shader-background"
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-black">
-			<span className="flex flex-col gap-4 md:items-start border-4 w-full">
-				<AnimatedText
-					text={["You", "Found Us!"]}
-					className="md:text-[12rem] text-6xl font-light tracking-tighter capitalize font-chivo-mono"
-				/>
-				<p className="text-4xl font-manrope font-light text-black dark:text-white">
-					We are a one-stop digital solution for all your business
-					needs.
-				</p>
+		<main className="flex min-h-screen flex-col items-center justify-center bg-transparent dark:bg-black">
+			<div className="absolute w-full h-full">
+				<ShaderBackground />
+			</div>
+			<span className="flex flex-col gap-4 md:items-start border-4 w-full z-20">
+				<Hero>
+					<Hero.Title>
+						<AnimatedText />
+					</Hero.Title>
+					<p className="text-4xl font-manrope font-light text-black dark:text-white">
+						We are a one-stop digital solution for all your business
+						needs.
+					</p>
+				</Hero>
 			</span>
 		</main>
 	)
